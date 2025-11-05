101f1ee6784ab2d34372d840603da6e93fa824bc
cristian bravo <cristian.bravo@udea.edu.co>
2025-11-04 23:21:46 -0500

    Topologia dinamica de altas y bajas de routers y enlaces, actualizar costo. Se configuro red por archivo y generacion aleatoria reproducible
    Signed-off-by: cristian bravo <cristian.bravo@udea.edu.co>

--- Pract4/Red.cpp
+++ Pract4/Red.cpp
@@ -9,13 +9,102 @@ bool Red::agregarEnrutador(const string& id) {
     return true;
 }
 
+bool Red::eliminarEnrutador(const string& id) {
+    if (!routers.count(id)) return false;
+    for (auto& kv : routers) kv.second->eliminarVecino(id);
+    routers.erase(id);
+    return true;
+}
+
 bool Red::agregarEnlace(const string& a, const string& b, int costo, bool bidir) {
     if (!routers.count(a) || !routers.count(b)) return false;
-    auto* A = routers[a].get();
-    auto* B = routers[b].get();
-    A->nuevoVecino(B, costo);
-    if (bidir) B->nuevoVecino(A, costo);
+    routers[a]->nuevoVecino(routers[b].get(), costo);
+    if (bidir) routers[b]->nuevoVecino(routers[a].get(), costo);
     return true;
+}
+
+bool Red::eliminarEnlace(const string& a, const string& b, bool bidir) {
+    if (!routers.count(a) || !routers.count(b)) return false;
+    routers[a]->eliminarVecino(b);
+    if (bidir) routers[b]->eliminarVecino(a);
+    return true;
+}
+
+bool Red::actualizarCosto(const string& a, const string& b, int costo, bool bidir) {
+    if (!routers.count(a) || !routers.count(b)) return false;
+    return agregarEnlace(a, b, costo, bidir);
+}
+
+static bool leerLineaValida(ifstream& in, string& out) {
+    string s;
+    while (std::getline(in, s)) {
+        auto pos = s.find('#');
+        if (pos != string::npos) s = s.substr(0,pos);
+        auto notsp = s.find_first_not_of(" \t\r\n");
+        if (notsp == string::npos) continue;
+        auto last = s.find_last_not_of(" \t\r\n");
+        out = s.substr(notsp, last - notsp + 1);
+        if (!out.empty()) return true;
+    }
+    return false;
+}
+
+bool Red::cargarDesdeArchivo(const string& ruta) {
+    ifstream in(ruta);
+    if (!in) return false;
+
+    routers.clear();
+
+    string line;
+    if (!leerLineaValida(in, line)) return false;
+    {
+        stringstream ss(line);
+        int N=0, M=0;
+        if (!(ss >> N >> M)) return false;
+
+        vector<string> nombres;
+        while ((int)nombres.size() < N) {
+            if (!leerLineaValida(in, line)) return false;
+            stringstream sn(line);
+            string tok;
+            while (sn >> tok) {
+                nombres.push_back(tok);
+                if ((int)nombres.size() == N) break;
+            }
+        }
+        for (auto& name : nombres) agregarEnrutador(name);
+
+        int leidas = 0;
+        while (leidas < M && leerLineaValida(in, line)) {
+            stringstream se(line);
+            string u, v; int w;
+            if (!(se >> u >> v >> w)) return false;
+            agregarEnlace(u, v, w, true);
+            ++leidas;
+        }
+        if (leidas != M) return false;
+    }
+    return true;
+}
+
+void Red::generarAleatoria(int n, double densidad, int cmin, int cmax, unsigned seed) {
+    routers.clear();
+    for (int i=0;i<n;++i) {
+        string id(1, char('A'+i));
+        agregarEnrutador(id);
+    }
+    mt19937 rng(seed ? seed : random_device{}());
+    uniform_real_distribution<double> prob(0.0,1.0);
+    uniform_int_distribution<int> peso(cmin, cmax);
+    for (int i=0;i<n;++i){
+        for (int j=i+1;j<n;++j){
+            if (prob(rng) <= densidad) {
+                string a(1, char('A'+i));
+                string b(1, char('A'+j));
+                agregarEnlace(a,b,peso(rng),true);
+            }
+        }
+    }
 }
 
 void Red::recomputarTablas() {
--- Pract4/Red.h
+++ Pract4/Red.h
@@ -7,6 +7,9 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <random>
 #include <algorithm>
 
 using namespace std;
@@ -16,7 +19,14 @@ public:
     unordered_map<string, unique_ptr<Enrutador>> routers;
 
     bool agregarEnrutador(const string& id);
+    bool eliminarEnrutador(const string& id);
+
     bool agregarEnlace(const string& a, const string& b, int costo, bool bidir=true);
+    bool eliminarEnlace(const string& a, const string& b, bool bidir=true);
+    bool actualizarCosto(const string& a, const string& b, int costo, bool bidir=true);
+
+    bool cargarDesdeArchivo(const string& ruta);   // N M | N nombres | M lineas u v w
+    void generarAleatoria(int n, double densidad, int cmin, int cmax, unsigned seed=0);
 
     void recomputarTablas();
     pair<int, vector<string>> rutaMasCorta(const string& origen, const string& destino) const;
